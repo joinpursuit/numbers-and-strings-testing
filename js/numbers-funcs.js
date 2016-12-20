30bcac25b975d756a5bf451623d0464ee41dcae8
Crazy Kiwi <Nasita-Haque>
2016-12-20 17:52:04 -0500

    added test

--- js/numbers-funcs.js
+++ js/numbers-funcs.js
@@ -1,5 +1,41 @@
 const add = (a, b) => a + b;
 
+const filterEvens = (arr) => (arr.filter((val)=>(
+  val % 2 === 0 && typeof val === 'number'
+)))
+
+const secondSmallest = (arr) => {
+  let sorted = arr.sort((a,b)=> a-b )
+  console.log(sorted)
+  for(var i = 0; i < sorted.length; i++){
+    if(sorted[i] !== sorted[i+1]){
+      return sorted[i+1]
+    }
+  }
+}
+
+const uniqueNum = (arr) => {
+  var obj = {}
+  var uniqueArr = []
+  for(var i = 0; i < arr.length; i++){
+    if(obj[arr[i]]){
+      obj[arr[i]]++
+    } else {
+      obj[arr[i]] = 1
+    }
+  }
+  console.log(Object.keys(obj))
+  for(var key in obj){
+    if(obj[key] < 2){
+      uniqueArr.push(parseInt(key))
+    }
+  }
+  return uniqueArr
+}
+
 module.exports = {
-  add: add
+  add: add,
+  filterEvens: filterEvens,
+  secondSmallest: secondSmallest,
+  uniqueNum: uniqueNum
 };
--- test/string-tests.js
+++ test/string-tests.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 const capitalize = require('../js/strings-funcs').capitalize;
 
 describe('strings tests', () => {
-  it('capitalize() mshould capitalize all letters in a string', () => {
+  it('capitalize() should capitalize all letters in a string', () => {
     var result1 = capitalize('hello');
     var result2 = capitalize('world');
     expect(result1).equal('HELLO');
