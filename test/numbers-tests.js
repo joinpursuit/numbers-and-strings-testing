bc5fb46b0f3582a45ddcb8263c0154a7e84b5860
iliass <iliass24@gmail.com>
2016-12-20 15:13:50 -0500

    add test for function secondSmallest

--- test/numbers-tests.js
+++ test/numbers-tests.js
@@ -1,14 +1,24 @@
 const expect = require('chai').expect;
-const add = require('../js/numbers-funcs').add;
-
+const func = require('../js/numbers-funcs');
 describe('numbers tests', () => {
   it('add() should add two numbers', () => {
-    var result1 = add(1, 2);
-    var result2 = add(5, 11);
-    var result3 = add(-10, 33);
+    var result1 = func.add(1, 2);
+    var result2 = func.add(5, 11);
+    var result3 = func.add(-10, 33);
     expect(result1).equal(3);
     expect(result2).equal(16);
     expect(result3).equal(23);
+  });
+    it('secondSmallest() should return the second smallest', () => {
+    var result1 = func.secondSmallest([1,5,10]);
+    var result2 = func.secondSmallest([-100,2,1000,0]);
+    var result3 = func.secondSmallest([-13, -44, 33]);
+    expect(result1).equal(5);
+    expect(result2).equal(0);
+    expect(result3).equal(-13)
+    expect(result1).to.be.a('number');
+
+;
   });
   //Add more tests here:
 });
