21f16db720d5985d73a6db9d8a27822a62826344
Ambar Gutierrez <ambar.grz@gmaail.com>
2016-12-20 17:57:04 -0500

    carlos and Ambar

--- js/numbers-funcs.js
+++ js/numbers-funcs.js
@@ -1,5 +1,32 @@
 const add = (a, b) => a + b;
+//filterEvens takes an array (that could include any data type) and returns an array of only the even numbers
+const filterEvens = function (arr){
+  var arrEven = []
+  for(var i = 0; i < arr.length; i++){
+    arr[i]= parseInt(arr[i]);
+    if(arr[i] % 2 === 0){
+      arrEven.push(arr[i])
+    }
+
+  }
+      return arrEven
+}
+
+// secondSmallest returns the second smallest number in an array
+
+
+const secondSmallest = function(arr){
+  arr.sort(function(a,b){
+    return a-b
+  })
+  return arr[1]
+}
+
+// uniqueNums takes an array of numbers and returns only the unique numbers
+
 
 module.exports = {
-  add: add
+  add: add,
+  filterEvens: filterEvens,
+  secondSmallest: secondSmallest,
 };
--- test/numbers-tests.js
+++ test/numbers-tests.js
@@ -1,5 +1,7 @@
 const expect = require('chai').expect;
 const add = require('../js/numbers-funcs').add;
+const filterEvens = require('../js/numbers-funcs').filterEvens;
+const secondSmallest = require('../js/numbers-funcs').secondSmallest;
 
 describe('numbers tests', () => {
   it('add() should add two numbers', () => {
@@ -11,4 +13,22 @@ describe('numbers tests', () => {
     expect(result3).equal(23);
   });
   //Add more tests here:
+  it('it should return an arreay of only even numbers', () => {
+    var resulteven = filterEvens([1,2,5,6,2,5]);
+    var resulteven1 = filterEvens(['a', 'b', 2, 3, "6"]);
+    expect(resulteven).to.eql([2,6,2]);
+    expect(resulteven1).to.eql([2, 6]);
+  })
+  it('it should return the second smallest number in an array', () => {
+    var resultSecondSmallest = secondSmallest([1,2,3,4,5,6]);
+    var resultSecondSmallest1 = secondSmallest([6,5,3,2,1]);
+    expect(resultSecondSmallest).to.eql(2);
+    expect(resultSecondSmallest1).to.eql(2);
+  })
+  it('should show only unique numbers on the array', () => {
+    var resultunique = uniqueNums([1,2,3,4,1]);
+    var resultunique1 = uniqueNums([1,2,3,4]);
+    expect(resultunique).to.eql([1,2,3,4]);
+    expect(resultunique1).to.eql([1,2,3,4]);
+  })
 });
