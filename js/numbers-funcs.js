277dd3375412acbdd02e1d55e5f8b3c85891a8a0
sfu <sfu.concepts@gmail.com>
2016-12-20 17:49:07 -0500

    Salina and Chris' Work

--- js/numbers-funcs.js
+++ js/numbers-funcs.js
@@ -1,5 +1,49 @@
 const add = (a, b) => a + b;
+const filterEvens = (arr) => {
+	return arr.filter(isEven)
+}
+
+const secondSmallest = (arr) => {
+	arr = arr.filter((val) => { 
+		return (typeof val) === 'number'
+	})
+	var min = arr[0]
+	var secMin = Infinity
+
+	for(var i = 0; i < arr.length; i++){
+		min > arr[i] ? min = arr[i] : null
+	}
+
+	for(var i = 0; i < arr.length; i++ ){
+		if(arr[i] > min && secMin > arr[i]){
+			secMin = arr[i]
+		}
+	}
+
+	return secMin != Infinity ? secMin : NaN;
+}
+
+const uniqueNums = (arr) => {
+		arr = arr.filter((val) => { 
+		return (typeof val) === 'number'
+	})
+	var newArr = []
+
+	for(var i = 0; i < arr.length; i++){
+		if(newArr.indexOf(arr[i]) === -1){
+			newArr.push(arr[i])
+		}
+	}
+	return newArr
+}
 
 module.exports = {
-  add: add
+  add: add,
+  filterEvens: filterEvens,
+  secondSmallest: secondSmallest,
+  uniqueNums : uniqueNums
 };
+
+function isEven(num){
+	return num % 2 === 0
+}
--- test/numbers-tests.js
+++ test/numbers-tests.js
@@ -1,5 +1,8 @@
 const expect = require('chai').expect;
 const add = require('../js/numbers-funcs').add;
+const filterEvens = require('../js/numbers-funcs').filterEvens;
+const secondSmallest = require('../js/numbers-funcs').secondSmallest;
+const uniqueNums = require('../js/numbers-funcs').uniqueNums;
 
 describe('numbers tests', () => {
   it('add() should add two numbers', () => {
@@ -9,6 +12,40 @@ describe('numbers tests', () => {
     expect(result1).equal(3);
     expect(result2).equal(16);
     expect(result3).equal(23);
-  });
-  //Add more tests here:
+  })
+
+  it('filterEvens() should filter out even numbers', () => {
+  	var result1 = filterEvens([7, 5]);
+  	var result2 = filterEvens([-4, 10]);
+  	var result3 = filterEvens([-3, 6]);
+  	var result4 = filterEvens(['hello', 3, 8])
+    expect(result1).eql([]);
+  	expect(result2).eql([-4, 10]);
+  	expect(result3).eql([6]);
+  	expect(result4).eql([8]);
+  })
+
+    it('secondSmallest() should return smallest number in array', () => {
+  	var result1 = secondSmallest([7, 5, 3]);
+  	var result2 = secondSmallest([-4, 10, -2, 1]);
+  	var result3 = secondSmallest([1, 3])
+  	var result4 = secondSmallest(['hello', 3, 8]);
+  	var result5 = secondSmallest(['hello','hotdog']);
+    expect(result1).equal(5);
+  	expect(result2).equal(-2);
+  	expect(result3).equal(3);
+  	expect(result4).equal(8);
+  	expect(result5).eql(NaN)
+  })
+
+    it('uniqueNums() should return an array of unique numbers', () => {
+  	var result1 = uniqueNums([7, 5, 3]);
+  	var result2 = uniqueNums([-4, 10, -2, 1, 1]);
+  	var result4 = uniqueNums(['hello', 3, 8]);
+  	var result5 = uniqueNums(['hello','hotdog', 2, 2]);
+    expect(result1).eql([7, 5, 3]);
+  	expect(result2).eql([-4, 10, -2, 1]);
+  	expect(result4).eql([3, 8]);
+  	expect(result5).eql([2])
+  })
 });
